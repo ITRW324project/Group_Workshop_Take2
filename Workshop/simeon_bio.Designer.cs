bed1e18faefad802e944d4d746ca4c5bebf7ac03
Simeon <itrw324project@gmail.com>
2017-08-07 15:55:08 +0200

    First Stable Commit

---
+++ Workshop/simeon_bio.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Workshop
+{
+    partial class simeon_bio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 45);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "My name is Simeon.\r\nI am a student.";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Type in a number between 0-9";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(36, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calculate random number";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // simeon_bio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 391);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "simeon_bio";
+            this.Text = "Simeon_bio";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Workshop/simeon_bio.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Workshop
+{
+    public partial class simeon_bio : Form
+    {
+        public simeon_bio()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random n = new Random();
+            int usernumber = Convert.ToInt32(textBox2.Text);
+            int[] array = new int[usernumber];
+            int sum = 0;
+            int count = 0;
+            int smallest = 50;
+            for (int i = 0; i < usernumber ; i++)
+            {
+                count += i;
+                sum += array[i];
+                array[i] = n.Next(10, 50);
+                smallest = array[i];
+                MessageBox.Show(Convert.ToString(array[i]));
+                if(array[i]< smallest)
+                {
+                    smallest = array[i];
+                }
+            }
+            double mean = sum / count;
+            double volg = 0;
+            for(int i = 0; i < usernumber; i++)
+            {
+                count += i;
+                volg += Math.Sqrt(array[i] - mean);
+            }
+            double stndDev = volg / count;
+            MessageBox.Show("hallo " + Convert.ToString(smallest));
+            MessageBox.Show("Die is die stdDev " + Convert.ToString(stndDev));
+            
+        }
+    }
+}
